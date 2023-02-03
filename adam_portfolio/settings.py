06533731d87254b8c3685ec93a002b8a6b9c80e0
aboczek <adam.boczekwork@gmail.com>
2023-02-03 20:49:29 +0000

    Removing secret key from github and replacing it

--- adam_portfolio/settings.py
+++ adam_portfolio/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+if os.path.isfile("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qohx!_bfl=v)fn)3-sq(6$=6uo_oo7628aw)fz4#)n3wf^3*l$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
