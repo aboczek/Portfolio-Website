bd35ce5471cca76ebea44866dcdc55b77a3a83de
aboczek <adam.boczekwork@gmail.com>
2023-04-01 17:52:17 +0000

    Create db details model and register it

--- portfolio_project/models.py
+++ portfolio_project/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Details(models.Model):
+    """
+    Database model for details.
+    """
+    full_name = models.CharField(max_length=50)
+    age = models.PositiveBigIntegerField()
+    nationality = models.CharField(max_length=50)
+    languages = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
