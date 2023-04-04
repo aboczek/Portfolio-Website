e9f6f49fc4516b8fc2012442ae9c024499a871bb
aboczek <adam.boczekwork@gmail.com>
2023-04-04 20:09:39 +0000

    Add DetailsForm into forms.py

--- portfolio_project/forms.py
+++ portfolio_project/forms.py
@@ -3,6 +3,25 @@ from .models import Details, Skills, Projects
 
 
 class SkillsForm(forms.ModelForm):
+    """
+    Skills form.
+    """
     class Meta:
+        """
+        Meta for skills form.
+        """
         model = Skills
         fields = ['skill',]
+
+
+class DetailsForm(forms.ModelForm):
+    """
+    Details form.
+    """
+    class Meta:
+        """
+        Meta for details form.
+        """
+        model = Details
+        fields = ['full_name', 'age', 'nationality', 'languages', 'address']
+        
