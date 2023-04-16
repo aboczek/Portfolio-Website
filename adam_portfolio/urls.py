e0abd6ff4e100fc00171793a64c353d55296e846
aboczek <adam.boczekwork@gmail.com>
2023-04-16 18:27:35 +0000

    Removing login.html and views for it

--- adam_portfolio/urls.py
+++ adam_portfolio/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('aboutme/', views.about_me, name="about_me"),
     path('projects/', views.projects, name="projects"),
-    # path('login/', views.login_panel, name="login_panel"),
     path('front/', views.front_panel, name="front_panel"),
     path('edit-skill/<skill_id>', views.edit_skill, name='edit'),
     path('delete-skill/<skill_id>', views.delete_skill, name='delete'),
