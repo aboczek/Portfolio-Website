e0aa9e2eac0c7d2245f6d75e5bd92d8cb429904a
aboczek <adam.boczekwork@gmail.com>
2023-04-21 16:08:41 +0000

    Trying to deploy heroku app

--- adam_portfolio/settings.py
+++ adam_portfolio/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOPMENT')
+DEBUG = False
 
 ALLOWED_HOSTS = ["portfolio-website-aboczek.herokuapp.com", "localhost"]
 
