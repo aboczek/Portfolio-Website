10e8a5c73b0a95f96741b3f56b92d9bafa49fda1
aboczek <adam.boczekwork@gmail.com>
2023-04-26 20:23:57 +0000

    Changing urls in project and app from aboutme/ to about-me/

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('aboutme/', views.about_me, name="about_me"),
+    path('about-me/', views.about_me, name="about_me"),
     path('projects/', views.projects, name="projects"),
     path('front/', views.front_panel, name="front_panel"),
     path('edit-skill/<skill_id>', views.edit_skill, name="edit"),
